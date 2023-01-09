abb43944095547c0ac34c8baf1b4b33a00697c96
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2023-01-09 23:57:04 +0530

    Fix version in Linux

--- scripts/download/Linux.sh
+++ scripts/download/Linux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
-version=$(curl -s $GITHUB_API_URL/repos/porjo/youtubeuploader/releases/latest | jq -r .tag_name)
+version=$(curl -sL https://github.com/porjo/youtubeuploader/releases/latest/ | grep "<title>Release" | awk -F " " '{print $2}')
 echo ::group::Downloading youtubeuploader $version for Linux
 aria2c -x 16 $GITHUB_SERVER_URL/porjo/youtubeuploader/releases/download/$version/youtubeuploader_${version}_Linux_x86_64.tar.gz
 mkdir youtubeuploader
