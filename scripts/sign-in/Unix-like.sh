218c6071ac6c4ec67647630a04674a695c6a1153
AnimMouse <git@animmouse.com>
2023-04-06 16:25:36 +0800

    fix: Add quotes on variables

--- scripts/sign-in/Unix-like.sh
+++ scripts/sign-in/Unix-like.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 echo ::group::Signing in to youtubeuploader
-echo $client_secrets | base64 -d > $RUNNER_TEMP/client_secrets.json
-echo $request_token | base64 -d > $RUNNER_TEMP/request.token
+echo "$client_secrets" | base64 -d > $RUNNER_TEMP/client_secrets.json
+echo "$request_token" | base64 -d > $RUNNER_TEMP/request.token
 echo ::endgroup::
