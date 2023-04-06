cca86c87708fc06dccb9f0fdf99cb094cbb30b94
AnimMouse <git@animmouse.com>
2023-04-06 16:49:31 +0800

    fix: Use native commands for download and extracting

--- scripts/download/Unix-like.sh
+++ scripts/download/Unix-like.sh
@@ -2,8 +2,6 @@
 set -eu
 echo ::group::Downloading youtubeuploader $version for $RUNNER_OS
 if [ $RUNNER_OS = macOS ]; then os=Darwin; else os=Linux; fi
-aria2c -x 16 -q $GITHUB_SERVER_URL/porjo/youtubeuploader/releases/download/$version/youtubeuploader_${version}_${os}_x86_64.tar.gz
-mkdir youtubeuploader
-tar -xf youtubeuploader_${version}_${os}_x86_64.tar.gz -C youtubeuploader youtubeuploader
-rm youtubeuploader_${version}_${os}_x86_64.tar.gz
+mkdir -p youtubeuploader
+wget -qO- $GITHUB_SERVER_URL/porjo/youtubeuploader/releases/download/$version/youtubeuploader_${version}_${os}_x86_64.tar.gz | tar -xz -C youtubeuploader youtubeuploader
 echo ::endgroup::
--- scripts/download/Windows.ps1
+++ scripts/download/Windows.ps1
@@ -1,7 +1,8 @@
 $ErrorActionPreference = 'Stop'
+$ProgressPreference = 'SilentlyContinue'
 Set-StrictMode -Version Latest
 Write-Host ::group::Downloading youtubeuploader $env:version for Windows
-aria2c -x 16 -q $env:GITHUB_SERVER_URL/porjo/youtubeuploader/releases/download/$env:version/youtubeuploader_${env:version}_Windows_x86_64.zip
-7z e youtubeuploader_${env:version}_Windows_x86_64.zip youtubeuploader.exe -oyoutubeuploader -r
+Invoke-WebRequest $env:GITHUB_SERVER_URL/porjo/youtubeuploader/releases/download/$env:version/youtubeuploader_${env:version}_Windows_x86_64.zip -OutFile youtubeuploader_${env:version}_Windows_x86_64.zip
+Expand-Archive youtubeuploader_${env:version}_Windows_x86_64.zip youtubeuploader
 Remove-Item youtubeuploader_${env:version}_Windows_x86_64.zip
 Write-Host ::endgroup::
