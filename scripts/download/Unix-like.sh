b75bd67ec052bf8cb603925d7bbdc2de965bda4b
Anim Mouse <git@animmouse.com>
2024-12-27 14:01:19 +0800

    fix: Use new release file naming

--- scripts/download/Unix-like.sh
+++ scripts/download/Unix-like.sh
@@ -3,5 +3,5 @@ set -eu
 echo ::group::Downloading youtubeuploader $version for $RUNNER_OS
 if [ $RUNNER_OS = macOS ]; then os=Darwin; else os=Linux; fi
 mkdir -p youtubeuploader
-wget -qO- $GITHUB_SERVER_URL/porjo/youtubeuploader/releases/download/$version/youtubeuploader_${version}_${os}_x86_64.tar.gz | tar -xz -C youtubeuploader youtubeuploader
+wget -qO- $GITHUB_SERVER_URL/porjo/youtubeuploader/releases/download/$version/youtubeuploader_${version/v}_${os}_amd64.tar.gz | tar -xz -C youtubeuploader youtubeuploader
 echo ::endgroup::
--- scripts/download/Windows.ps1
+++ scripts/download/Windows.ps1
@@ -2,7 +2,8 @@ $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 Set-StrictMode -Version Latest
 Write-Host ::group::Downloading youtubeuploader $env:version for Windows
-Invoke-WebRequest $env:GITHUB_SERVER_URL/porjo/youtubeuploader/releases/download/$env:version/youtubeuploader_${env:version}_Windows_x86_64.zip -OutFile youtubeuploader_${env:version}_Windows_x86_64.zip
-Expand-Archive youtubeuploader_${env:version}_Windows_x86_64.zip youtubeuploader
-Remove-Item youtubeuploader_${env:version}_Windows_x86_64.zip
+$version = $env:version.Substring(1)
+Invoke-WebRequest $env:GITHUB_SERVER_URL/porjo/youtubeuploader/releases/download/v$version/youtubeuploader_${version}_Windows_amd64.zip -OutFile youtubeuploader_${version}_Windows_amd64.zip
+Expand-Archive youtubeuploader_${version}_Windows_amd64.zip youtubeuploader
+Remove-Item youtubeuploader_${version}_Windows_amd64.zip
 Write-Host ::endgroup::
