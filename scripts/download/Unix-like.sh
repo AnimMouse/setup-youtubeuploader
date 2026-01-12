eb55cdcdd819ab3e4aecae0ac934e85d1aa07834
Anim Mouse <git@animmouse.com>
2026-01-12 12:22:20 +0800

    feat: Add support for arm64 macOS and Linux

--- scripts/download/Unix-like.sh
+++ scripts/download/Unix-like.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -eu
-echo ::group::Downloading youtubeuploader $version for $RUNNER_OS
+echo ::group::Downloading youtubeuploader $version for $RUNNER_OS $RUNNER_ARCH
 if [ $RUNNER_OS = macOS ]; then os=Darwin; else os=Linux; fi
+if [ $RUNNER_ARCH = ARM64 ]; then arch=arm64; else arch=amd64; fi
 mkdir -p youtubeuploader
-wget -qO- $GITHUB_SERVER_URL/porjo/youtubeuploader/releases/download/$version/youtubeuploader_${version/v}_${os}_amd64.tar.gz | tar -xz -C youtubeuploader youtubeuploader
+wget -qO- $GITHUB_SERVER_URL/porjo/youtubeuploader/releases/download/$version/youtubeuploader_${version/v}_${os}_${arch}.tar.gz | tar -xz -C youtubeuploader youtubeuploader
 echo ::endgroup::
